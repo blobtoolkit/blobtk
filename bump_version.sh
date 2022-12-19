226097240f15321e88e9619f77aff24e2b815999
Richard Challis <richard.challis@sanger.ac.uk>
2022-12-19 11:37:37 +0000

    use cargo bump to update rust version

---
+++ bump_version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LEVEL=$1
+
+if [ -z "$LEVEL" ]; then
+  echo "Usage: ./bump_version.sh major|minor|patch"
+  exit 1;
+fi
+
+if [ ! -z "$(git status --porcelain)" ]; then 
+  echo "Commit changes before running this script"
+  exit 1;
+fi
+
+cd rust &&
+
+cargo bump $LEVEL &&
+
+cd - &&
+
+git commit -a
+
+bump2version $LEVEL --allow-dirty
