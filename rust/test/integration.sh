e7335b47f180d7ca1b3d86c10b37f4bcc8d5d17d
Richard Challis <richard.challis@sanger.ac.uk>
2023-02-22 09:50:23 +0000

    add cargo/maturin builds to integration tests

--- rust/test/integration.sh
+++ rust/test/integration.sh
@@ -2,6 +2,10 @@
 
 echo "Runnning integration tests"
 
+CMD="cargo build --release"
+printf "\nrunning command\n$CMD\n\n"
+$CMD || exit 1
+
 CMD="./target/release/blobtk depth -b test/test.bam -O test/test.bed"
 printf "\nrunning command\n$CMD\n\n"
 $CMD || exit 1
@@ -13,6 +17,16 @@ $CMD || exit 1
 CMD="./target/release/blobtk filter -i test/test.list -b test/test.bam -f test/reads_1.fq.gz -r test/reads_2.fq.gz -F"
 printf "\n\nrunning command\n$CMD\n\n"
 $CMD || exit 1
+
+CMD="rm -f ./target/wheels/blobtk-*.whl && 
+    maturin build --release &&
+    yes | pip uninstall blobtk &&
+    yes | pip install ./target/wheels/blobtk-*.whl"
+printf "\nrunning command\n$CMD\n\n"
+rm -f ./target/wheels/blobtk-*.whl &&
+    maturin build --release &&
+    yes | pip uninstall blobtk &&
+    yes | pip install ./target/wheels/blobtk-*.whl || exit 1
 
 CMD="./test/depth.py"
 printf "\n\nrunning command\n$CMD\n\n"
